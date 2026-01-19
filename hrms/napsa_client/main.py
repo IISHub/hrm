e2a6327a4aea892ed5fbe717e81f80b90858e3e3
simwawatim <simwawatimothy65@gmail.com>
2026-01-19 13:44:37 +0000

    added update status

--- hrms/napsa_client/main.py
+++ hrms/napsa_client/main.py
@@ -26,6 +26,25 @@ class NapsaClient():
                 "data": data
             })
             frappe.local.response.http_status_code = http_status
+            
+            
+    def send_response_list(self, status="success", message="", data=None, status_code=200, http_status=200):
+        response_payload = {
+            "status_code": status_code,
+            "status": status,
+            "message": message
+        }
+
+        if data is not None:
+            if isinstance(data, dict) and "success" in data and "data" in data:
+                response_payload["data"] = data["data"]
+                response_payload["pagination"] = data.get("pagination", {})
+            else:
+                response_payload["data"] = data
+
+        frappe.local.response = frappe._dict(response_payload)
+        frappe.local.response.http_status_code = http_status
+        
 
     def get_saved_token(self):
         token = frappe.db.get_value("Napsa Keys", {}, "token")

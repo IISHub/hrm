8f3d4d080e17c9fef3d5f026fd8bfd07f665ea58
simwawatim <simwawatimothy65@gmail.com>
2026-02-08 12:45:28 +0000

    added mock for napsa

--- hrms/napsa_client/auth/auth.py
+++ hrms/napsa_client/auth/auth.py
@@ -17,7 +17,7 @@ def save_new_token(token: str):
 
 
 @frappe.whitelist(allow_guest=False)
-def get_token(mock: bool = False):
+def get_token(mock: bool = True):
     try:
         if mock:
             random_token = uuid.uuid4()
@@ -45,7 +45,7 @@ def get_token(mock: bool = False):
             "scope": "*"
         }
 
-        response = requests.post(url, json=payload, headers=headers, timeout=15)
+        response = requests.post(url, json=payload, headers=headers, timeout=40)
         response.raise_for_status()
 
         data = response.json()
